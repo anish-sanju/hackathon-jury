b790b389ca531445b8df03772faec6a49d2e1cf0
Anish-Shobith <anish.shobith19@gmail.com>
2023-04-24 20:59:41 +0530

    port for railway
    Signed-off-by: Anish-Shobith <anish.shobith19@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ const app = express()
 app.use('/', pageRouter);
 app.use('/teams', teamRouter);
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
